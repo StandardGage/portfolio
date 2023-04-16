8ba4acd39db24154953645713eb5e4cec937814c
StandardGage <50509562+StandardGage@users.noreply.github.com>
2023-04-16 13:47:49 -0400

    changed contact message

--- src/containers/Contact.tsx
+++ src/containers/Contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
             <div onClick={(e)=>e.stopPropagation()} className="flex flex-col justify-between bg-[#dfdede] dark:bg-[#2d2d30] rounded-xl shadow-md w-3/5 h-3/5 sm:w-4/5 p-5">
                 <div className="font-proza sm:text-2xl md:text-6xl lg:text-6xl">Contact Me</div>
-                <div className="font-telex text-4xl">Thanks for stopping by!</div>
+                <div className="font-telex text-4xl">Feel free to reach out!</div>
 
                 <div className="flex text-2xl flex-col m-2">
                 <div className="font-taviraj">Email: <a className="underline" href="mailto:gageschu@udel.edu">gageschu@udel.edu</a></div>
