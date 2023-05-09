101143de652f8ec3fa9d52fdb7c1147a684efa92
StandardGage <gagedolph@yahoo.com>
2023-05-09 00:28:56 -0400

    improved toggles

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,9 +5,9 @@ import { MdOutlineDarkMode, MdOutlineLightMode } from 'react-icons/md'
 export default function ThemeToggle(props:any) {
     return (
         <motion.button
-        initial={{scale:1.5, opacity:0}}
-        whileHover={{scale:2}}
-        whileTap={{scale:1}}
+        initial={{opacity:0}}
+        whileHover={{scale:1.3}}
+        whileTap={{scale:.8}}
         whileInView={{opacity:1}}
         onClick={() => {
             var theme = localStorage.getItem('theme') === 'dark' ? 'dark' : 'light';
@@ -20,7 +20,7 @@ export default function ThemeToggle(props:any) {
                 document.documentElement.classList.remove('dark')
                 document.documentElement.classList.add('light')
             }
-        }}><MdOutlineDarkMode className='block dark:hidden' id='darkIcon'></MdOutlineDarkMode>
-        <MdOutlineLightMode className='hidden dark:block' id='lightIcon'></MdOutlineLightMode></motion.button>
+        }}><MdOutlineDarkMode size={30} className='block dark:hidden' id='darkIcon'></MdOutlineDarkMode>
+        <MdOutlineLightMode size={30} className='hidden dark:block' id='lightIcon'></MdOutlineLightMode></motion.button>
     )
 }
