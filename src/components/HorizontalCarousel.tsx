309c813dc17aa717c3f49994d24b52422a6ac909
StandardGage <50509562+StandardGage@users.noreply.github.com>
2023-06-01 14:28:29 -0400

    remove files for astro addition

--- src/components/HorizontalCarousel.tsx
+++
@@ -1,8 +0,0 @@
-export default function HorizontalCarousel(props: { children: any[]; }) {
-    
-    return (
-        <div className="flex space-x-4 justify-start items-center overflow-scroll">
-            {props.children}
-        </div>
-    )
-}
