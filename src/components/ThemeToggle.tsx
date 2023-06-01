84ebd277ed25ee46b075b2f694c8894efd81897d
StandardGage <50509562+StandardGage@users.noreply.github.com>
2023-06-01 13:54:27 -0400

    fixed themetoggle pressing

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -7,9 +7,8 @@ export default function ThemeToggle(props:any) {
         <motion.button
         initial={{opacity:0}}
         whileHover={{scale:1.3}}
-        whileTap={{scale:.8}}
-        whileInView={{opacity:1}}
-        onClick={() => {
+        whileTap={{scale:0.9}}
+        onTap={()=>{
             var theme = localStorage.getItem('theme') === 'dark' ? 'dark' : 'light';
             if (theme === 'light') {
                 localStorage.setItem('theme', 'dark')
@@ -20,7 +19,9 @@ export default function ThemeToggle(props:any) {
                 document.documentElement.classList.remove('dark')
                 document.documentElement.classList.add('light')
             }
-        }}><div className='block text-3xl dark:hidden grayscale sm:grayscale-0 md:grayscale-0 hover:grayscale-0 duration-300' id='darkIcon'>{"🌙"}</div>
+        }}
+        whileInView={{opacity:1}}
+        ><div className='block text-3xl dark:hidden grayscale sm:grayscale-0 md:grayscale-0 hover:grayscale-0 duration-300' id='darkIcon'>{"🌙"}</div>
         <div className='hidden grayscale sm:grayscale-0 md:grayscale-0 hover:grayscale-0 text-3xl dark:block hover:text-primary duration-300' id='lightIcon'>{"☀️"}</div></motion.button>
     )
 }
