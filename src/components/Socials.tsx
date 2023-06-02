65b000548e028701072da9aff3222cb61cbb20d0
StandardGage <50509562+StandardGage@users.noreply.github.com>
2023-06-02 13:39:29 -0400

    change resume

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,5 +1,5 @@
 import { socials } from "../layouts/portfolio"
-
+import pdf from "../assets/Gage_Schuster_Resume.pdf"
 import { MailOutlined, PrinterOutlined } from '@ant-design/icons';
 
 export function Social(props: {children:any}) {
@@ -28,7 +28,7 @@ export default function Socials() {
                 <svg className="hover:fill-primary dark:hover:fill-primary duration-300 dark:fill-white" height="2rem" version="1.1" viewBox="0 0 512 512" width="2rem" xmlns="http://www.w3.org/2000/svg"><g id="_x32_7-artstation"><g><path d="M26,365.896l38.925,67.261c7.695,15.208,23.354,25.617,41.55,25.617h258.357l-53.591-92.878H26z     M478.44,341.094L327.989,77.938c-7.785-14.664-23.265-24.713-41.099-24.713h-80.023l232.919,405.186l36.843-63.818    C478.35,391.697,495.64,367.706,478.44,341.094z M273.131,299.903L168.576,118.856L64.02,299.903H273.131z"/></g></g><g id="Layer_1"/></svg>
                 </a>
             </Social>
-            <Social><a target="_blank" href="https://docs.google.com/document/d/13OUqTOunyhWkEVVocGyN95YlEgKotKBheBdvKYyhIfI/edit?usp=share_link">
+            <Social><a target="_blank" href={pdf}>
                 <PrinterOutlined style={{fontSize:'2rem'}}></PrinterOutlined>
                 </a>
             </Social>
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Socials from "../components/Socials";
 import { Modal, ConfigProvider } from "antd";
+import pdf from "../assets/Gage_Schuster_Resume.pdf"
 
 
 export default function Contact() {
@@ -30,7 +31,7 @@ export default function Contact() {
                     <div className="font-telex text-4xl text-center">Feel free to reach out!</div>
                     <div>
                         <div className="font-taviraj text-2xl">Email: <a className="underline" href="mailto:gageschu@udel.edu">gageschu@udel.edu</a></div>
-                        <div className="font-taviraj text-2xl">Resume: <a className="underline" target="_blank" href="https://docs.google.com/document/d/13OUqTOunyhWkEVVocGyN95YlEgKotKBheBdvKYyhIfI/edit">document</a></div>
+                        <div className="font-taviraj text-2xl">Resume: <a className="underline" target="_blank" href={pdf}>document</a></div>
                     </div>
                     <Socials></Socials>
                 </div>
