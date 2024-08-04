7ff37671810c4686184f577e94cee2b96671de8b
StandardGage <gagedolph@yahoo.com>
2024-08-03 22:39:52 -0500

    remove bottlenecks

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,29 +1,31 @@
-import { Variant, Variants, motion } from "framer-motion";
+import { type Variant, type Variants, motion } from "framer-motion";
 import { useState } from "react";
 
-const front:Variants = {
+const front: Variants = {
   rest: { rotateY: 0, zIndex: 5 },
-  flipped: { rotateY: 180, zIndex: 0},
+  flipped: { rotateY: 180, zIndex: 0 },
 };
 
-const back:Variants = {
+const back: Variants = {
   rest: { rotateY: -180, zIndex: 0 },
   flipped: { rotateY: 0, zIndex: 5 },
 };
 
 export default function Card(props: any) {
   const [flipped, setFlipped] = useState(false);
-  const [small, setSmall] = useState(false)//useState(document.body.clientWidth < 812);
+  const [small, setSmall] = useState(false); //useState(document.body.clientWidth < 812);
 
   const flipCard = () => {
     setFlipped(!flipped);
   };
 
+  console.log(props.images);
+
   return (
     <motion.div
-      onClick={() =>flipCard()}
-      onHoverStart={()=>small ? null : setFlipped(true)}
-      onHoverEnd={()=>small ? null : setFlipped(false)}
+      onClick={() => flipCard()}
+      onHoverStart={() => (small ? null : setFlipped(true))}
+      onHoverEnd={() => (small ? null : setFlipped(false))}
       className={`min-w-[314px] min-h-[314px] relative flex justify-center`}
     >
       <motion.div
@@ -43,15 +45,21 @@ export default function Card(props: any) {
         }}
         className="shadow-md absolute dark:text-black rounded-xl min-w-full min-h-full"
       >
-        <div className="p-1 rounded-t-xl text-accent h-fit bg-secondary dark:dark:bg-[#37373a]">{props.front}</div>
+        <div className="p-1 rounded-t-xl text-accent h-fit bg-secondary dark:dark:bg-[#37373a]">
+          {props.front}
+        </div>
       </motion.div>
       <motion.div
         animate={flipped ? "flipped" : "rest"}
         initial="rest"
         variants={back}
         transition={{ duration: 0.6 }}
-        style={{ transformStyle: "preserve-3d", backfaceVisibility: "hidden", WebkitBackfaceVisibility: "hidden" }}
-        className="overflow-y-auto overflow-x-wrap absolute bg-inherit backdrop-brightness-95 dark:bg-[#37373a] rounded-xl shadow-md min-w-full min-h-full max-h-[314px]"
+        style={{
+          transformStyle: "preserve-3d",
+          backfaceVisibility: "hidden",
+          WebkitBackfaceVisibility: "hidden",
+        }}
+        className="overflow-y-auto overflow-x-wrap absolute bg-inherit bg-neutral-200 dark:bg-[#37373a] rounded-xl shadow-md min-w-full min-h-full max-h-[314px]"
       >
         {props.back}
       </motion.div>
--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -8,25 +8,42 @@ import { useState } from "react";
 import CardModal from "../components/CardModal";
 
 export default function Education() {
-    const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-    return (
-        <Section id="education" title="My Education...">
-            <ol className="flex flex-row flex-wrap gap-10 justify-center">
-                {education.schools.map((school, i) => {
-                    return (
-                        <>
-                            
-                        <CardModal width={'clamp(100px, 70vw, 800px)'} height={'clamp(100px, 30vh, 800px)'} image={school.image} title={school.schoolName} subtitle={school.title} date={school.date}>
-                            <Collapsible label={"Classes"}><div className="mx-9 text-sm text-left font-telex whitespace-pre">{'• '+school.classes.join('\n• ')}</div></Collapsible>
-                            <Collapsible label={"Clubs/Activities"}><div className="mx-9 text-sm text-left font-telex whitespace-pre">{'• '+school.clubs.join('\n• ')}</div></Collapsible>
-                            <Collapsible label={"Awards"}><div className="mx-9 text-sm text-left font-telex whitespace-pre">{'• '+school.awards.join('\n• ')}</div></Collapsible>
-                        </CardModal>
-                        </>
-                        
-                    )
-                })}
-            </ol>
-        </Section>
-    )
+  return (
+    <Section id="education" title="My Education...">
+      <ol className="flex flex-row flex-wrap gap-10 justify-center">
+        {education.schools.map((school, i) => {
+          return (
+            <>
+              <CardModal
+                width={"clamp(100px, 70vw, 800px)"}
+                height={"clamp(100px, 30vh, 800px)"}
+                image={school.image.src}
+                title={school.schoolName}
+                subtitle={school.title}
+                date={school.date}
+              >
+                <Collapsible label={"Classes"}>
+                  <div className="mx-9 text-sm text-left font-telex whitespace-pre">
+                    {"• " + school.classes.join("\n• ")}
+                  </div>
+                </Collapsible>
+                <Collapsible label={"Clubs/Activities"}>
+                  <div className="mx-9 text-sm text-left font-telex whitespace-pre">
+                    {"• " + school.clubs.join("\n• ")}
+                  </div>
+                </Collapsible>
+                <Collapsible label={"Awards"}>
+                  <div className="mx-9 text-sm text-left font-telex whitespace-pre">
+                    {"• " + school.awards.join("\n• ")}
+                  </div>
+                </Collapsible>
+              </CardModal>
+            </>
+          );
+        })}
+      </ol>
+    </Section>
+  );
 }
